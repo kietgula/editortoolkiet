64b02dda2b406c18f1d2ec84e34368d48962a1c6
frdome <61832885+kietgula@users.noreply.github.com>
2024-05-08 10:33:52 +0700

    Update RoadMaker.cs

--- RoadMaker.cs
+++ RoadMaker.cs
@@ -17,11 +17,18 @@ public class RoadMaker : EditorWindow
 
     private GameObject roadParent = null;
 
+    [Tooltip("Tick cái này thì tool mới thao tác được")]
     private bool isMakingRoad = false;
     
+    [Tooltip("Tự động focus vào điêm cuối của đường đi mỗi khi tạo đường")]
     private bool isAutoFocusLastRoadPoint = false;
     
+    [Tooltip("Điểm bắt đầu của đường đi")]
+    [SerializeField]
     private Vector3 lastRoadPoint = Vector3.zero;
+    
+    [Tooltip("Độ rộng của đường đi")]
+    private float roadWidth = 5f;
 
     private void Awake()
     {
@@ -58,6 +65,38 @@ public class RoadMaker : EditorWindow
         
         isMakingRoad = EditorGUILayout.Toggle("Is Making Road", isMakingRoad);
         
+        isAutoFocusLastRoadPoint = EditorGUILayout.Toggle("Is Auto Focus Last Road", isAutoFocusLastRoadPoint);
+        
+        roadWidth = EditorGUILayout.FloatField("Road Width", roadWidth);
+        if (GUILayout.Button("Go to last Road & Reset last Point"))  
+        {
+            if (roadParent != null)
+            {
+                if (roadParent.transform.childCount > 0)
+                {
+                    SceneView.lastActiveSceneView.pivot = roadParent.transform.GetChild(roadParent.transform.childCount - 1).gameObject.transform.position;
+                    SceneView.lastActiveSceneView.Focus();
+                    lastRoadPoint = Vector3.zero;
+                }
+                else
+                {
+                    lastRoadPoint = Vector3.zero;
+                    Debug.LogError("Road Parent is empty");
+                }
+            }
+        }
+        if (GUILayout.Button("Delete last road"))
+        {
+            if (roadParent != null)
+            {
+                if (roadParent.transform.childCount > 0)
+                {
+                    DestroyImmediate(roadParent.transform.GetChild(roadParent.transform.childCount - 1).gameObject);
+                    lastRoadPoint = Vector3.zero;
+                }
+            }
+        }
+        
         EditorGUILayout.Space();
         EditorGUILayout.LabelField("####################################");
         EditorGUILayout.Space();
@@ -70,14 +109,6 @@ public class RoadMaker : EditorWindow
             lastRoadPoint = Vector3.zero;
         }
         EditorGUILayout.EndHorizontal();
-        
-        if (GUILayout.Button("Go to last Road Point"))  
-        {
-            SceneView.lastActiveSceneView.pivot = lastRoadPoint;
-            SceneView.lastActiveSceneView.Focus();
-        }
-        
-        isAutoFocusLastRoadPoint = EditorGUILayout.Toggle("Is Auto Focus Last Road", isAutoFocusLastRoadPoint);
     }
 
     private void OnSceneGUI(SceneView sceneView)
@@ -103,7 +134,7 @@ public class RoadMaker : EditorWindow
                     {
                         GameObject road = GameObject.CreatePrimitive(PrimitiveType.Cube);
                         road.transform.position = (lastRoadPoint + hit.point) / 2;
-                        road.transform.localScale = new Vector3(5, 1f, Vector3.Distance(lastRoadPoint, hit.point) + 1f);
+                        road.transform.localScale = new Vector3(roadWidth, 1f, Vector3.Distance(lastRoadPoint, hit.point) + 1f);
                         road.transform.LookAt(hit.point);
                         road.transform.parent = roadParent.transform;
                         lastRoadPoint = hit.point;
@@ -118,6 +149,7 @@ public class RoadMaker : EditorWindow
             }
             
             Repaint();  
+            
         }
     }
 }
